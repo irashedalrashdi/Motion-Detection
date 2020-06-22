9a4be589d5435b1c13aacb92de027788c916873e
Rashed Alrashdi <40270845+RashedAlrashdi@users.noreply.github.com>
2020-06-22 17:52:02 +0300

    Add files via upload

---
+++ pyMotinDetection/motion-detection.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+Import time
+
+Input_Pin = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Input_Pin, GPIO.IN)
+
+for x in range(0, 500):
+    if GPIO.input(Input_Pin):
+	print('Detection')
+    else:
+	print('No detection')
+    time.sleep(0.1)
+	
+GPIO.cleanup()
+print('Done')
